d058fce7f4ffa72596c4f64508d0b88e1901d4aa
umarifti <umariftikhar2004@gmail.com>
2026-07-21 19:01:10 +0500

    Stage 1 (root and health endpoint)

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({
+    name: 'Task API',
+    version: '1.0',
+    endpoints: ['/tasks'],
+  });
+});
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok',
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
